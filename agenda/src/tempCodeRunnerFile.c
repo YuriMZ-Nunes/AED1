f95fe3c2d3335d64a2f005520dd9cf0bd294eb81
YuriMZ-Nunes <yuri.m.z.nunes@gmail.com>
2022-09-24 17:22:27 -0300

    commit

--- agenda/src/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-if((void **)pFirst == NULL){ //verifica se é o primeiro cadastro
-		*(void **)pFirst = infos; //atualiza o ponteiro para o primeiro
-		*(int *)(infos + ((sizeof(char) * 10) + (sizeof(int) * 2))) = 0;
-		*(int *)(infos + ((sizeof(char) * 10) + (sizeof(int) * 2) + sizeof(int))) = 0;
-		
-	}
