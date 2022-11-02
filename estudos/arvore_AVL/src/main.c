95ddaa8c01aec431da50e854fb118afc21ebd5ac
YuriMZ-Nunes <yuri.m.z.nunes@gmail.com>
2022-11-02 16:21:55 -0300

    commit

--- estudos/arvore_AVL/src/main.c
+++ estudos/arvore_AVL/src/main.c
@@ -7,7 +7,7 @@ typedef struct{
 	int height;
 }NO;
 
-NO* newNode(int x){
+NO* newNode(int x){ //função de criação de um novo nó
 	NO *new = malloc(sizeof(NO));
 
 	if(!new){
@@ -22,7 +22,7 @@ NO* newNode(int x){
 	return new;
 }
 
-int bigger(int a, int b) {
+int bigger(int a, int b) { //recebe a altura e retorna a maior
 	if(a > b){
 		return a;
 	}
@@ -30,18 +30,59 @@ int bigger(int a, int b) {
 	return b;
 }
 
-int nodeHeight(NO *node) {
+int nodeHeight(NO *node) {  //retorna a altura do nó. caso seja nulo retorna -1
 	if(!node)
 		return -1;
 
 	return node->height;
 }
 
-int balance(NO *node){
+int balance(NO *node){ //calcula o fator de balanceamento do nó
 	if(node)
 		return(nodeHeight(node->left) - nodeHeight(node->right));
 	
 	return 0;
+}
+
+NO* leftRotation(NO *root){ //rotação para a esquerda
+	NO *newRoot, *children;
+
+	newRoot = root->right; 
+	children = newRoot->left;
+
+	newRoot->left = root;
+	root->right = children;
+
+	root->height = bigger(nodeHeight(root->left), nodeHeight(root->right)) + 1;
+	newRoot->height = bigger(nodeHeight(newRoot->left), nodeHeight(newRoot->right)) + 1;
+
+	return newRoot;
+}
+
+NO* rightRotation(NO *root){ //rotação para direita
+	NO *newRoot, *children;
+
+	newRoot = root->left;
+	children = newRoot->right;
+
+	newRoot->right = root;
+	root->left = children;
+
+	root->height = bigger(nodeHeight(root->left), nodeHeight(root->right)) + 1;
+	newRoot->height = bigger(nodeHeight(newRoot->left), nodeHeight(newRoot->right)) + 1;
+
+	return newRoot;
+
+}
+
+NO* rightLeftRotation(NO *root){ //rotação esquerda direita
+	root->right = rightRotation(root->right);
+	return leftRotation(root);
+}
+
+NO* leftRightRotation(NO *root){ //rotação direita esquerda
+	root->left = leftRotation(root->left);
+	return rightRotation(root);
 }
 
 int main() {
