379d5671f96219948f3ccbea030018f675f795ae
YuriMZ-Nunes <yuri.m.z.nunes@gmail.com>
2022-11-05 22:44:45 -0300

    commit

--- exeProva3-correcao/src/main.c
+++ exeProva3-correcao/src/main.c
@@ -19,10 +19,10 @@ void list(NODE *root, int level);
 
 int main(){
 	NODE *root = NULL;
-	int num, numRotacao;
+	int num;
 
 	while(1){
-		printf("\n\n\t1. Inserir\n\t2. Giro a esquerda\n\t3. Giro a direita \n\t4. Listar\n\t5. Buscar\n\t6. Sair\n\tEscolha: ");
+		printf("\n\t1. Inserir\n\t2. Giro a esquerda\n\t3. Giro a direita \n\t4. Listar\n\t5. Buscar\n\t6. Remover\n\t7. Sair\n\tEscolha: ");
 		switch(getchar()){
 			case '1':
 				printf("\nDigite um valor: ");
@@ -32,24 +32,28 @@ int main(){
 				break;
 			case '2':
 				printf("\nDigite um numero: ");
-				scanf("%d", &numRotacao);
-				root = searchValue(root, numRotacao, 0);
+				scanf("%d", &num);
+				printf("\n\t--ROTACAO A ESQUERDA FEITA--\n");
+				root = searchValue(root, num, 0);
 				getchar();
 				break;
 			case '3':
 				printf("\nDigite um numero: ");
-				scanf("%d", &numRotacao);
-				root = searchValue(root, numRotacao, 1);
+				scanf("%d", &num);
+				printf("\n\t--ROTACAO A DIREITA FEITA--\n");
+				root = searchValue(root, num, 1);
 				getchar();
 				break;
 			case '4':
+				printf("\n\t--ARVORE COMPLETA--\n");
 				list(root, 1);
+				printf("\n");
 				getchar();
 				break;
 			case '5':
 				printf("\nDigite um numero: ");
-				scanf("%d", &numRotacao);
-				searchValue(root, numRotacao, 2);
+				scanf("%d", &num);
+				searchValue(root, num, 2);
 				getchar();
 				break;
 			case '6':
@@ -71,26 +75,26 @@ int main(){
 }
 
 NODE* newNode(int num){
-	NODE *new = malloc(sizeof(NODE));
+	NODE *new = malloc(sizeof(NODE)); //aloca espaço para um novo nó
 
 	if(!new){
 		printf("ERROR");
 	}else{
-		new->num = num;
-		new->left = NULL;
-		new->right = NULL;
-		printf("\n\nINSERIDO\n\n");
+		new->num = num; //atribui o valor do novo nó
+		new->left = NULL; //atribui nulo para o nó da esquerda
+		new->right = NULL; //atribui nulo para o nó da direita
+		printf("\n\t--NUMERO INSERIDO--\n");
 	}
 	return new;
 }
 NODE* push(NODE *root, int num){
 	if(root == NULL)
-		return newNode(num);
+		return newNode(num); //retorna a criação de um novo nó
 	else
 		if(num < root->num)
-			root->left = push(root->left, num);
+			root->left = push(root->left, num); //percorre para esquerda caso o numero seja menor que o nó anterior
 		else if(num > root->num)
-			root->right = push(root->right, num);
+			root->right = push(root->right, num); //percorre para direita caso o numero seja maior que o nó anterior
 		else
 			printf("\n\nERRO!!\n\n");
 	return root;
@@ -98,38 +102,40 @@ NODE* push(NODE *root, int num){
 
 void list(NODE *root, int level){
 	if(root){
-		list(root->right, level  + 1);
+		list(root->right, level  + 1); //percorre até o nó mais a direita
 		printf("\n\n");
 
-		for(int i = 0; i < level; i++)
+		for(int i = 0; i < level; i++) //adiciona espaços para formar o desenho da arvore
 			printf("\t");
 		
-		printf("%d", root->num);
-		list(root->left, level + 1);
+		printf("%d", root->num); 
+		list(root->left, level + 1); //percorre até o nó mais a esquerda
 	}
 }
 
-NODE* searchValue(NODE *root, int numRotacao, int rotateOption){
+NODE* searchValue(NODE *root, int num, int rotateOption){
 	if(root){
-		if(numRotacao > root->num)
-			root->right = searchValue(root->right, numRotacao, rotateOption);
-		else if(numRotacao < root->num)
-			root->left = searchValue(root->left, numRotacao, rotateOption);
-		else if(numRotacao == root->num){
+		if(num > root->num)
+			root->right = searchValue(root->right, num, rotateOption); //percorre para direita enquanto o numero procurado é maior q o numero do nó
+		else if(num < root->num)
+			root->left = searchValue(root->left, num, rotateOption); //percorre para esquerda enquanto o numero procurado é menor q o numero do nó
+		else if(num == root->num){ //verifica se encontrou
 			if(rotateOption == 0)
-				root = rotateLeft(root);
+				root = rotateLeft(root); //opção 0: rotação a esquerda no nó procurado
 			else if(rotateOption == 1)
-				root = rotateRight(root);
+				root = rotateRight(root); //opção 1: rotação a direita no nó procurado
 			else if(rotateOption == 2)
-				printf("\n o numero %d esta cadastrado!", root->num);
-			else
-				root = pop(root);
+				printf("\n\t--NUMERO %d ENCONTRADO--\n", root->num); //opção 3: apenas buscar o nó
+			else{
+				root = pop(root); //opção 4: apagar o nó procurado
+				printf("\n\t--NUMERO %d REMOVIDO--\n", num);
+			}
 		}
 	}
 	return root;
 }
 
-NODE* rotateLeft(NODE *root){
+NODE* rotateLeft(NODE *root){ //rotação a esquerda
 	NODE *aux, *son;
 	
 	aux = root->right;
@@ -141,7 +147,7 @@ NODE* rotateLeft(NODE *root){
 	return aux;
 }
 
-NODE* rotateRight(NODE *root){
+NODE* rotateRight(NODE *root){ //rotação a direita
 	NODE *aux, *son;
 
 	aux = root->left;
@@ -153,7 +159,7 @@ NODE* rotateRight(NODE *root){
 	return aux;		
 }
 
-void exitProgram(NODE *root){
+void exitProgram(NODE *root){ //libera todos os nós da memória
 	if(root){
 		exitProgram(root->left);
 		exitProgram(root->right);
@@ -161,7 +167,7 @@ void exitProgram(NODE *root){
 	}
 }
 
-NODE* pop(NODE *root){
+NODE* pop(NODE *root){ //remove o nó escolhido e suas folhas
 	if(root){
 		pop(root->left);
 		pop(root->right);
